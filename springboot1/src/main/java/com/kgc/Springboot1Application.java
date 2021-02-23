0053a18213dc364e6b8a1a833f46d8ee0089c64c
sophiaxm <61382097+sophiaxm@users.noreply.github.com>
2021-02-23 10:43:27 +0800

    Update Springboot1Application.java

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -10,6 +10,7 @@ public class Springboot1Application {
 
     public static void main(String[] args) {
 	//组员a修改代码
+	//组员b修改代码
         SpringApplication.run(Springboot1Application.class, args);
     }
 
